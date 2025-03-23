253efd64ef422e8d55ca77cc4f51e5aada79c308
Thiago Bel <narbnooxiinn@gmail.com>
2025-03-23 19:19:59 -0300

    Add files via upload

--- scripts/timers/timer.js
+++ scripts/timers/timer.js
@@ -1,18 +1,26 @@
-const age = document.getElementById('ageInfo');
-const time = document.getElementById('timeInPro');
+let age = document.getElementById('ageInfo');
+let time = document.getElementById('timeInPro');
 
-const dataInicial = new Date("2023-11-09");
-const dataAtual = new Date();
-const diferencaMs = dataAtual - dataInicial;
-const dias = Math.floor(diferencaMs / (1000 * 60 * 60 * 24));
-time.innerHTML = dias + " Dias";
+let dataInicial = new Date("2023-11-10"); //um dia a mais
+let dataNascimento = new Date("2010-11-09"); //um dia a mais tbm
 
-const dataNascimento = new Date("2010-11-09");
-const diferencaIdadeMs = dataAtual - dataNascimento;
-const idade = Math.floor(diferencaIdadeMs / (1000 * 60 * 60 * 24 * 365.25));
-age.innerHTML = idade + " Anos";
+function atualizarInformacoes() {
+    let dataAtual = new Date();
+    let diferencaMs = dataAtual - dataInicial;
+    let dias = Math.floor(diferencaMs / (1000 * 60 * 60 * 24));
+    time.innerHTML = dias + " Dias";
 
-if (dataAtual.getDate() === dataNascimento.getDate() && dataAtual.getMonth() === dataNascimento.getMonth()) {
-    age.style.color = "Red";
-    age.title = "Meu aniversario é hoje🥳";
+    let diferencaIdadeMs = dataAtual - dataNascimento;
+    let idade = Math.floor(diferencaIdadeMs / (1000 * 60 * 60 * 24 * 365.25));
+    age.innerHTML = idade + " Anos";
+
+    if (dataAtual.getDate() === dataNascimento.getDate() && dataAtual.getMonth() === dataNascimento.getMonth()) {
+        age.style.color = "Red";
+        age.title = "Meu aniversário é hoje🥳";
+    } else {
+        age.style.color = "";
+        age.title = "";
+    }
 }
+
+setInterval(atualizarInformacoes, 100);
